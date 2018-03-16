5d6eabbbca1660dac7ea28a364ddd770bda0efe2
ZoeLeBlanc <zgleblanc@gmail.com>
2018-03-16 17:35:21 -0400

    first commit of new app

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import routes from '../routes';
+
+
+const App = () => (
+    <div>
+        { routes }
+    </div>
+);
+
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    devtool: 'eval-source-map',
+    entry: [
+        'babel-polyfill',
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
+        'react-hot-loader/patch',
+        path.join(__dirname, 'src/index.js')
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    output: {
+        path: path.join(__dirname, '/build/'),
+        filename: '[name].js',
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+          {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader', 'eslint-loader']
+            },
+            {
+                test: /\.(css)$/,
+                use: ExtractTextPlugin.extract({
+                  fallback: 'style-loader',
+                  use: 'css-loader'
+                })
+             },
+             {
+                test: /\.(png|jpg|gif)$/,
+                exclude: /(node_modules)/,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {}
+                  }
+                ]
+              }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: 'src/index.tpl.html',
+          inject: 'body',
+          filename: 'index.html'
+        }),
+        new ExtractTextPlugin("styles.css"),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.DefinePlugin({
+          'process.env.NODE_ENV': JSON.stringify('development')
+        })
+    ],
+};
